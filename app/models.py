48e633b61c7cda07169db23e3b6b109e5538d6b1
Manikandan Ramakrishnan <manikandan.ramakrishnan@inmar.com>
2016-12-01 16:28:10 -0500

    Added is_active and date_modified fields

--- app/models.py
+++ app/models.py
@@ -6,12 +6,20 @@ class Base(db.Model):
 
     id = db.Column(db.BigInteger, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
 
 
 class Monitors(Base):
     __tablename__ = 'active_monitors'
 
-    dir_name = db.Column(db.String, nullable=False)
+    dir_path = db.Column(db.String, nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
 
-    def __init__(self, dir_name):
-        self.dir_name = dir_name
+    def __init__(self, dir_path, is_active = True):
+        """
+        :string dir_path: Directory path to be monitored
+        :boolean is_active: Is the directory active for monitoring
+        """
+        self.dir_path = dir_path
+        self.is_active = is_active
